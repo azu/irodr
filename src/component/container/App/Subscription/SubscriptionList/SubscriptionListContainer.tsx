a1785080c8a09727a0f674297014475f9b7be9dc
azu <azuciao@gmail.com>
2017-09-26 22:02:38 +0900

    chore(list): add data-feedId

--- src/component/container/App/Subscription/SubscriptionList/SubscriptionListContainer.tsx
+++ src/component/container/App/Subscription/SubscriptionList/SubscriptionListContainer.tsx
@@ -93,6 +93,7 @@ export class SubscriptionListContainer extends BaseContainer<SubscriptionListCon
                     "is-currentItem": isCurrentItem
                 })}
                 onClick={onClick}
+                data-feedId={item.id.toString()}
             >
                 <img className="SubscriptionListContainer-itemImage" src={item.iconUrl} width={16} height={16} />
                 <Link className="SubscriptionListContainer-itemLink">
