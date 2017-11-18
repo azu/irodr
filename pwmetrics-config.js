f4e6e24907d80818de2da750b223e70e8c62c9ab
azu <azuciao@gmail.com>
2017-11-18 10:27:42 +0900

    chore(react): Update React

--- pwmetrics-config.js
+++ pwmetrics-config.js
@@ -3,11 +3,15 @@ let secret = {};
 try {
     secret = require("./pwmetrics.secret");
 } catch (error) {}
+// pwmerics_url=http://localhost:13245/?ci yarn run  pwmerics
+const specificURL = process.env.pwmerics_url;
+const targetURL = specificURL ? specificURL : !isCI ? "http://localhost:13245/?ci" : "http://irodr.netlify.com/?ci";
 module.exports = {
-    url: !isCI ? "http://localhost:13245/?ci" : "http://irodr.netlify.com/?ci",
+    url: targetURL,
     sheets: {
         type: "GOOGLE_SHEETS", // sheets service type. Available types: GOOGLE_SHEETS
         options: {
+            // https://docs.google.com/spreadsheets/d/16Z3v5sP05Z9KsOeRrnh3rRdUMXTIUDcE4aKhDvfo-mk/edit
             spreadsheetId: "16Z3v5sP05Z9KsOeRrnh3rRdUMXTIUDcE4aKhDvfo-mk",
             tableName: "data"
         }
