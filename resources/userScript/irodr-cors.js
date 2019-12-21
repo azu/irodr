93cd43266ec2c2baad1a3b8e991fc1249debe155
azu <azuciao@gmail.com>
2019-12-21 19:04:15 +0900

    feat(userScript): add CORS proxy

---
+++ resources/userScript/irodr-cors.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name        irodr: Replace fetch with gm_xmlhttprequest
+// @namespace   https://irodr.netlify.com/
+// @match       https://irodr.netlify.com/
+// @grant       GM_xmlhttpRequest
+// @version     1.0
+// @author      azu
+// @description Direct API Request without CORS Error
+// @run-at document-start
+// ==/UserScript==
+// Hook
+localStorage.setItem("REACT_APP_INOREADER_API_BASE_URL", "https://www.inoreader.com/reader");
+// Fetch API with CORS
+const fromEntries = e => e.reduce((acc, [k, v]) => ((acc[k] = v), acc), {});
+const parseHeader = h =>
+    fromEntries(
+        h
+            .split("\n")
+            .filter(Boolean)
+            .map(l => l.split(":").map(tok => tok.trim()))
+    );
+unsafeWindow.fetch = (input, init = {}) => {
+    return new Promise(res => {
+        if (init.headers instanceof Headers) {
+            init.headers = fromEntries(Array.from(init.headers.entries()));
+        }
+        init.data = init.body;
+        init = Object.assign(
+            {
+                method: "GET",
+                headers: {}
+            },
+            init,
+            {
+                url: input,
+                responseType: "blob"
+            }
+        );
+        GM_xmlhttpRequest(
+            Object.assign({}, init, {
+                onload: xhr => {
+                    xhr.headers = parseHeader(xhr.responseHeaders);
+                    res(new Response(xhr.response, Object.assign({}, init, xhr)));
+                },
+                onerror: xhr => {
+                    console.log("err", xhr);
+                    res(new Response(xhr.response, Object.assign({}, init, xhr)));
+                }
+            })
+        );
+    });
+};
+console.log("window.fetch ", unsafeWindow.fetch);
