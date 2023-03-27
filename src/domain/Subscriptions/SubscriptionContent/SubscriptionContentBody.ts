5f675bb9e3d54b7778e4f2fd3c26ce5dc8b5cd31
azu <azuciao@gmail.com>
2023-03-27 11:53:17 +0900

    fix

--- src/domain/Subscriptions/SubscriptionContent/SubscriptionContentBody.ts
+++ src/domain/Subscriptions/SubscriptionContent/SubscriptionContentBody.ts
@@ -2,25 +2,36 @@
 import { ValueObject } from "ddd-base";
 import type { Enclosure } from "../../../infra/api/StreamContentsResponse";
 
+export type SubscriptionContentBodyArgs = { enclosures?: Enclosure[]; content: string };
+
 export class SubscriptionContentBody extends ValueObject<{}> {
-    constructor(private body: { enclosures?: Enclosure[]; content: string }) {
+    constructor(private body: SubscriptionContentBodyArgs) {
         super(body);
     }
 
     get HTMLString(): string {
+        const hasImageInContent = this.body.content.includes("<img");
+        if (hasImageInContent) {
+            return this.body.content;
+        }
+        // If enclosures has image, show it
+        // But, prevent to show duplicated image in content
         const attachments =
             this.body.enclosures
                 ?.map((enclosure) => {
                     if (enclosure.type.startsWith("image/")) {
-                        return `<img src="${enclosure.href}" alt="" />`;
+                        return `<img class="SubscriptionContentBody-enclosure" src="${enclosure.href}" alt="" />`;
                     }
                     return "";
                 })
                 .join("") ?? "";
-        return this.body.content + (attachments ? `<div>${attachments}</div>` : "");
+        return (
+            this.body.content +
+            (attachments ? `<div class="SubscriptionContentBody-enclosures">${attachments}</div>` : "")
+        );
     }
 
-    toJSON() {
+    toJSON(): SubscriptionContentBodyArgs {
         return {
             content: this.body.content,
             enclosures: this.body.enclosures
