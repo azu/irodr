e4ca4a9616f3b78a5183dc904dba07925bf95501
azu <azuciao@gmail.com>
2023-07-22 17:12:34 +0900

    fix

--- tools/no-console-proxy.mjs
+++ tools/no-console-proxy.mjs
@@ -3,5 +3,5 @@ import fs from 'node:fs/promises';
 import path from "node:path";
 const __dirname = new URL('.', import.meta.url).pathname;
 const proxyCode = await fs.readFile(path.join(__dirname, '../netlify/edge-functions/cors-proxy.ts'), 'utf8');
-const hasConsole = /console\.\w\(/;
+const hasConsole = /console\.\w+\(/;
 assert(!hasConsole.test(proxyCode), 'console.log() found in proxy code');
